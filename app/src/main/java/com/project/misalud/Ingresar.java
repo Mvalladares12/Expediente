e48daeb520895b9f740f88658df7163e281895b3
Frank Dav <frankhernz77@gmail.com>
2024-12-07 19:22:27 -0600

    Mejoras en los campos y nuevas opciones de registro

--- app/src/main/java/com/project/misalud/Ingresar.java
+++ app/src/main/java/com/project/misalud/Ingresar.java
@@ -3,7 +3,6 @@ package com.project.misalud;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -11,19 +10,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
-import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -126,11 +119,11 @@ public class Ingresar extends AppCompatActivity {
                     if (documentSnapshot.exists()) {
                         String role = documentSnapshot.getString("role");
 
-                        if ("doctor".equals(role)) {
+                        if ("Doctor".equals(role)) {
                             Intent adminIntent = new Intent(Ingresar.this, MainActivity.class);
                             startActivity(adminIntent);
                             finish();
-                        } else if ("user".equals(role)) {
+                        } else if ("Usuario".equals(role)) {
                             Intent userIntent = new Intent(Ingresar.this, MainActivity.class);
                             startActivity(userIntent);
                             finish();
